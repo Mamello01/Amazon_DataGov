6ee55a2c0c867c8e28d2d94fd87dd47abb6c33a6
Mamello01 <Melmafoks@gmail.com>
2026-04-30 14:23:39 +0200

    feat: create and load geolocation raw table

--- database/00_raw_tables.sql
+++ database/00_raw_tables.sql
@@ -13,3 +13,17 @@ SELECT *
 FROM raw.customers_raw
 LIMIT 5;
 
+CREATE TABLE IF NOT EXISTS raw.geolocation_raw (
+    geolocation_zip_code_prefix INTEGER,
+    geolocation_lat NUMERIC(10,8),
+    geolocation_lng NUMERIC(11,8),
+    geolocation_city TEXT,
+    geolocation_state CHAR(2)
+);
+
+SELECT COUNT(*) 
+FROM raw.geolocation_raw;
+
+SELECT * 
+FROM raw.geolocation_raw 
+LIMIT 10;
